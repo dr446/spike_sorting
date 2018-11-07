f798aaf7423d42dd803bb24d118d0c03fc32ea4b
Ethan Sorrell <ethantsorrell@gmail.com>
2018-11-07 11:51:06 +0000

    edit matched filter

--- matched_filt.m
+++ matched_filt.m
@@ -12,11 +12,15 @@ for i = 1:256
     
 end
 %find max value and index
-[mxv ind] = max(filtered(:));
+%[mxv ind] = max(filtered(:));
 %convert index: r = template, c = timestampe, p = channel
-[r,c,p] = ind2sub(size(filtered),ind);
+%[r,c,p] = ind2sub(size(filtered),ind);
 %output vector of max values for each template
-maxvec = max(filtered,[],[2 3]);
+%maxvec = max(filtered,[],[2 3]);
+filt_summed = sum(filtered,3);
+[mxv ind] = max(filt_summed(:));
+[r,c,p] = ind2sub(size(fil_summed),ind);
+maxvec = max(filt_summed,[],2);
 toc;
 plot(maxvec)
 end
